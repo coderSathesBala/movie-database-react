5863005f894bb5043607aa55c5295c6e804a2f4d
Sathes Balasingam <sathes352@gmail.com>
2019-02-27 21:23:15 -0500

    navBarPractice

---
+++ src/Home.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch ('https://api.themoviedb.org/3/genre/movie/list?api_key=04933c26041758065df384adb2cc7541&language=en-US')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.genres
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li> {item.name} </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+}
+
+export default Home;
