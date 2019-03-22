b14af8bddbb0d86f48cb2f2e02ca35f9e3fa6aee
Sathes Balasingam <sathes352@gmail.com>
2019-03-22 15:52:25 -0400

    Updates

---
+++ precache-manifest.6f780a653f3c642374e97833055bb2de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8285e2984c25ea93bad",
+    "url": "/movie-database-react/static/js/runtime~main.767b6159.js"
+  },
+  {
+    "revision": "af8edf0def7dbcf9d845",
+    "url": "/movie-database-react/static/js/main.7d29a808.chunk.js"
+  },
+  {
+    "revision": "a521be81f8d0bfa1a26b",
+    "url": "/movie-database-react/static/js/2.6429571a.chunk.js"
+  },
+  {
+    "revision": "af8edf0def7dbcf9d845",
+    "url": "/movie-database-react/static/css/main.efc637d5.chunk.css"
+  },
+  {
+    "revision": "4d2f7d539eebc0625a7bbda00af04d46",
+    "url": "/movie-database-react/index.html"
+  }
+];
